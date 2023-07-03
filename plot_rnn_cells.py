74c52c8ed06176c3dacb84cba5ffca1b4396ad43
Mia-Katrin Kvalsund <mia.kvalsund@gmail.com>
2023-07-03 15:08:11 +0200

    Create plot_rnn_cells.py

---
+++ plot_rnn_cells.py
@@ -0,0 +1,50 @@
+import torch
+import matplotlib.pyplot as plt
+import ratsimulator
+from PlaceCells import PlaceCells
+from dataloader import Dataset
+from SorscherRNN import SorscherRNN
+from tqdm import tqdm
+import numpy as np
+from utils import multiimshow
+import scipy
+from main import get_dataloader
+
+if __name__ == "__main__":
+    from localconfig import config
+    config.read("config")
+
+    # Model class must be defined somewhere
+    model = torch.load("model")
+    model.eval()
+
+    dataloader, dataset = get_dataloader(config)
+
+    # number of spatial samples across mini-batch and sequence length
+    nsamples = 100000
+    recurrent_activities = []
+    stacked_positions = []
+    for i, (v, p0, _, positions) in enumerate(dataloader):
+        recurrent_activities.append(model.g(v, p0).cpu().detach().numpy())
+        stacked_positions.append(positions[:,1:].cpu().detach().numpy())
+        if i*dataloader.batch_size*dataset.seq_len > nsamples:
+            break
+    # stack runs in mini-batch dimension
+    recurrent_activities = np.concatenate(recurrent_activities, axis=0)
+    stacked_positions = np.concatenate(stacked_positions, axis=0)
+    print("recurrent_activities =", recurrent_activities.shape)
+    print("stacked_positions =", stacked_positions.shape)
+    # flatten mini-batch and sequence length dimensions
+    recurrent_activities = recurrent_activities.reshape(-1, recurrent_activities.shape[-1])
+    stacked_positions = stacked_positions.reshape(-1, stacked_positions.shape[-1])
+    print("recurrent_activities =", recurrent_activities.shape)
+    print("stacked_positions =", stacked_positions.shape)
+
+    # Now we have positions and the correpsonding recurrent activities for each position
+    # We can use this to compute the firing field for some example recurrent cells
+
+    ratemaps = scipy.stats.binned_statistic_2d(*stacked_positions.T, recurrent_activities[:,:16].T, statistic='mean', bins=50)[0]
+    print("ratemaps =", ratemaps.shape)
+
+    multiimshow(ratemaps, figsize=(10,10));
+    plt.show()
