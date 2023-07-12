41a1af68b7192bc8efeb3ce68785fe6f2911c8f9
Mia-Katrin Ose Kvalsund <mia.kvalsund@gmail.com>
2023-07-12 21:31:32 +0200

    Main is changed to plot more and currently to only run Vemund's RNN

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import torch
 import matplotlib.pyplot as plt
-from SorscherRNN_cuda import SorscherRNN
+from SorscherRNN_original import SorscherRNN
 from tqdm import tqdm
 
 from dataloader import get_dataloader
@@ -13,14 +13,18 @@ if __name__ == "__main__":
     logger = Logger()
     logger.save_config(config)
 
-    model = SorscherRNN(
-        alpha=config.experiment.alpha,
-        beta=config.experiment.beta,
-        weight_decay=config.experiment.weight_decay,
-    ) 
+    model = SorscherRNN()
+    #    alpha=config.experiment.alpha,
+    #    beta=config.experiment.beta,
+    #    weight_decay=config.experiment.weight_decay,
+    #) 
     # move model to GPU if available
     if torch.cuda.is_available():
         model = model.to('cuda')
+    elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        # MacOS M1 chips have a specific torch speedup:
+        # https://towardsdatascience.com/installing-pytorch-on-apple-m1-chip-with-gpu-acceleration-3351dc44d67c
+        model = model.to('mps')
 
     dataloader, _ = get_dataloader(config)
 
@@ -47,13 +51,22 @@ if __name__ == "__main__":
             loss_history.append(loss)
         if i % config.training.save_interval == 0:
             logger.save_model(model)
+
+            # Remove this later 
+            # The handling of saving loss figure should be moved to logger
+            plt.figure()
+            plt.xlabel("Epoch")
+            plt.ylabel("Loss")
+            plt.plot(loss_history)
+            plt.savefig(logger.path + "/loss_figure" + str(i))
         if i > num_train_steps:
             break
 
     logger.save_model(model)
 
     # The handling of saving loss figure should be moved to logger
-    plt.xlabel("Time step")
+    plt.figure()
+    plt.xlabel("Epoch")
     plt.ylabel("Loss")
     plt.plot(loss_history)
     plt.savefig(logger.path + "/loss_figure")
